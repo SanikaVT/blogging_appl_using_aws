50f22bf3d65b867059121f0e1a6d1a136d7c3b2b
Minal Khona <mn977442@dal.ca>
2022-07-02 17:51:45 -0300

    Like Integration

--- frontend/src/components/blog/BlogCard.js
+++ frontend/src/components/blog/BlogCard.js
@@ -2,45 +2,64 @@ import { Stack, Typography, IconButton, Button, Paper } from "@mui/material";
 import * as React from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Box } from "@mui/system";
+import { getUserId } from "../../localStorage";
+import axios from "axios";
 
 export default function BlogCard({ handleMenu, item }) {
-  return (
-    <Paper sx={{ p: 2, mt: 2 }}>
-      <Stack
-        direction="row"
-        sx={{ mb: 3, justifyContent: "space-between", alignItems: "center" }}
-      >
-        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-          {item.author_id}
-        </Typography>
-        <Stack direction="row">
-          <Button
-            variant="contained"
-            size="small"
-            sx={{ margin: "auto", mr: 0.5 }}
-          >
-            Follow
-          </Button>
-          <IconButton onClick={handleMenu}>
-            <MoreVertIcon />
-          </IconButton>
-        </Stack>
-      </Stack>
-      <hr />
-      <Typography sx={{ fontWeight: "bold" }}>
-        {item.title}
-      </Typography>
-      <br />
-      <Typography>
-        {item.content}
-      </Typography>
-      <Stack direction="row" sx={{ alignItems: "center" }}>
-        <IconButton>
-          <FavoriteIcon sx={{ color: "red" }} />
-        </IconButton>
-        <Typography variant="body2">{item.likes_count}</Typography>
-      </Stack>
-    </Paper >
-  );
+
+    const likeBlog = (blogId) => {
+        console.log('BlogId:', blogId)
+        console.log('UserId:', getUserId())
+        axios({
+            method: 'put',
+            url: 'https://ahulfo14r5.execute-api.us-east-1.amazonaws.com/likeBlog',
+            data: {
+                blogId: blogId,
+                userId: getUserId()
+            }
+        }).then((res) => {
+            console.log(res.message);
+        }).catch(err => {
+            console.log('Error while calling like blog API: ', err)
+        });
+    };
+
+    return (
+        <Paper sx={{ p: 2, mt: 2 }}>
+            <Stack
+                direction="row"
+                sx={{ mb: 3, justifyContent: "space-between", alignItems: "center" }}
+            >
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                    {item.author_id}
+                </Typography>
+                <Stack direction="row">
+                    <Button
+                        variant="contained"
+                        size="small"
+                        sx={{ margin: "auto", mr: 0.5 }}
+                    >
+                        Follow
+                    </Button>
+                    <IconButton onClick={handleMenu}>
+                        <MoreVertIcon />
+                    </IconButton>
+                </Stack>
+            </Stack>
+            <hr />
+            <Typography sx={{ fontWeight: "bold" }}>
+                {item.title}
+            </Typography>
+            <br />
+            <Typography>
+                {item.content}
+            </Typography>
+            <Stack direction="row" sx={{ alignItems: "center" }}>
+                <IconButton>
+                    <FavoriteIcon sx={{ color: "red" }} onClick={() => likeBlog(item.blog_id)} />
+                </IconButton>
+                <Typography variant="body2">{item.likes_count}</Typography>
+            </Stack>
+        </Paper >
+    );
 }
